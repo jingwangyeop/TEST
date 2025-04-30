bddaf5f7584d114c1f8e92dd3d6c7f2570a52be9
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-04-30 15:37:39 +0900

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 from openai import OpenAI
-
+import time
 
 apikey = st.text_input("입력해라 :")
 
 client = OpenAI(api_key=apikey)
 
 
+time.sleep(2)
+
 gg = st.text_input("말해라 :")
 
+time.sleep(1)
 
 response = client.chat.completions.create(
     model = "gpt-4o-mini",
