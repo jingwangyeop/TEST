2efb0ee05425040e8d8576a9943d147b83c27445
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-04-30 15:21:55 +0900

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -9,7 +9,7 @@ apikey = st.text_input("입력해라 :")
 client = OpenAI(api_key=apikey)
 
 response = client.chat.completions.create(
-    model = "gpt-4.1-mini",
+    model = "gpt-4o-mini",
     messages = [
         {"role": "system", "content": "You are a helpful chat bot"}
     ]
