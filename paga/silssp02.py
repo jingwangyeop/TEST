cd9be68cdcbabf05a58103aa686f63222b797142
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-05-07 16:12:47 +0900

    Update silssp02.py

--- paga/silssp02.py
+++ paga/silssp02.py
@@ -1,1 +1,22 @@
-print("asDF")
+apikey = st.text_input("openai api key를 입력하세요 :", type = "password")
+
+client = OpenAI(api_key=apikey)
+
+@st.cache_data
+def what(gg):
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": gg}
+        ]
+    )
+    return response.choices[0].message.content
+
+
+
+
+if apikey != "":
+    gg = st.text_input("질문하세요:")
+    if gg:
+        a = what(gg)
+        st.write(a)
