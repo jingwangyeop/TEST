c0d473c755c5e97fa1ae10aa604d5d15734f04bb
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-05-07 15:18:53 +0900

    Update silssp01.py

--- silssp01.py
+++ silssp01.py
@@ -7,17 +7,20 @@ apikey = st.text_input("openai api key를 입력하세요 :", type = "password")
 client = OpenAI(api_key=apikey)
 
 
+@st.cache_data
+def what():
+    if apikey != "":
+        gg = st.text_input("질문하세요:")
+    
+        if gg:
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=[
+                    {"role": "system", "content": gg}
+                ]
+            )
+    
+            that = st.write(response.choices[0].message.content)
+        return that
 
-if apikey != "":
-    gg = st.text_input("질문하세요:")
-
-    if gg:
-        response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[
-                {"role": "system", "content": gg}
-            ]
-        )
-
-        st.write(response.choices[0].message.content)
-        
+what()
