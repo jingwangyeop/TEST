b7fa7b771d96263d1c6f195882b62727f9c1febe
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-05-07 19:21:30 +0900

    Update silssp02.py

--- pages/silssp02.py
+++ pages/silssp02.py
@@ -4,3 +4,4 @@ from openai import OpenAI
 apikey = st.text_input("OPENAI API KEY를 입력하세요 :", type = "password")
 client = OpenAI(api_key=apikey)
 
+st.chat_input(placeholder="Your message", *, key=None, max_chars=None, accept_file=False, file_type=None, disabled=False, on_submit=None, args=None, kwargs=None)
