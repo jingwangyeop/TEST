97baf3939ef4f2c82db772785a16fb6d27ce2d58
jingwangyeop <wlsrhkdduq2016@gmail.com>
2025-05-08 12:28:41 +0900

    Update silssp04.py

--- pages/silssp04.py
+++ pages/silssp04.py
@@ -7,3 +7,25 @@ client = OpenAI(api_key=apikey)
 st.title("Jin's chatPDF bot")
 pdf = st.file_uploader("PDF파일을 올려주세요")
 
+if pdf !=null :
+  prompt = st.text.input("PDF 내용에 대해 질문해주세요 :")
+
+if prompt !="":
+  response = client.responses.create(
+    model="gpt-4.1-mini",
+    input=[
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "input_file",
+            "file_id": pdf.id,
+          },
+          {
+            "type": "input_text",
+            "text": prompt,
+          },
+        ]
+      }
+    ]
+  )
